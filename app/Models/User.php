6119546219aed74fdd94df1fd5bbbb8e2609b3e9
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-10-02 20:33:10 +0700

    Refactor user model to use HasFactory trait, remove unused policies, and update dependencies in composer files

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-    use HasRoles;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
